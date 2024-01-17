a94671e103b5cd2ba110134948c5f318be4cd4bd
Lucas Alex <lucasalexti@gmail.com>
2024-01-16 21:49:03 -0300

    ajustes estruturais e retracking

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,13 +1,11 @@
-import Header from './ui/partials/Header';
-import NewTask from './ui/partials/NewTask';
-import Tasks from './ui/partials/Tasks';
+import Header from './partials/Header';
+import NewTask from './partials/NewTask';
 
 const Home = () => {
   return (
     <div className="h-[100dvh] flex flex-col">
       <Header />
       <NewTask />
-      <Tasks />
     </div>
   );
 };
